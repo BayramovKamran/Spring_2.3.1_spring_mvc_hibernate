0ea5b4717a65b8b46f7e218992292f646dcb0c7b
Kamran <kamran.07@icloud>
2023-09-11 16:08:05 +0300

    first commit

---
+++ src/main/java/web/service/UserServiceImpl.java
@@ -0,0 +1,46 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.dao.UserDAO;
+import web.model.User;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserDAO userDAO;
+
+    public UserServiceImpl(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @Override
+    @Transactional
+    public void saveUser(User user) {
+        userDAO.saveUser(user);
+    }
+
+    @Override
+    @Transactional
+    public void deleteUser(Long id) {
+        userDAO.deleteUser(id);
+    }
+
+    @Override
+    @Transactional
+    public void updateUser(User user) {
+        userDAO.updateUser(user);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userDAO.findAll();
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userDAO.getUserById(id);
+    }
+}
